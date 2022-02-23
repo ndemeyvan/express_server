0a239e016d45d056d127d8e4d3880059bc6b7c71
ndeme yvan <yvan.ndeme@papersoft.ng>
2022-02-23 17:33:45 +0100

    init push

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 
 // Home Response route
 app.get("/", (req, res) => {
-    res.send("Hello Seven Devs!");
+    res.send("Hello Seven Devs Maxime!");
   });
 
   // Server setup
