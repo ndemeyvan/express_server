a368a5d177f260aa0b4f003df7cfacdcc1635cc8
ndeme yvan <yvan.ndeme@papersoft.ng>
2022-02-23 16:46:06 +0100

    init push

---
+++ index.js
@@ -0,0 +1,16 @@
+
+
+// Requiring modules
+const express = require("express");
+const app = express();
+
+// Home Response route
+app.get("/", (req, res) => {
+    res.send("Hello Seven Devs!");
+  });
+
+  // Server setup
+app.listen(3000, () => {
+    console.log(
+  `Server listening on http://localhost:3000`);
+  });
