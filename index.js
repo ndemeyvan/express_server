10f444c082b800947ad5a4aa069856d956122a6d
ndeme yvan <yvan.ndeme@papersoft.ng>
2022-04-25 14:52:57 +0100

    commit

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 
 // Home Response route
 app.get("/", (req, res) => {
-    res.send("Welcome to the home page v4!");
+    res.send("Welcome to the home page v5!");
   });
 
   // Server setup
