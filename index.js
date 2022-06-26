6d1a2e96a4dbf56ecbe78b6bd05428e12746dbbe
ndemeyvan <ndemeyvan@gmail.com>
2022-06-26 12:10:48 +0200

    add kubectl command

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 
 // Home Response route
 app.get("/", (req, res) => {
-    res.send("Welcome to the home page v5!");
+    res.send("Hello production");
   });
 
   // Server setup
